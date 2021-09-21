2042a3735381b17fe42f7aa369908eb5292cf7ea
HQB92 <76698088+HQB92@users.noreply.github.com>
2021-09-21 15:07:06 -0300

    21-09-2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Footer from './components/footer';
+import Login from './components/login';
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Home from './components/home';
+import AsisAlumno from './components/asis-alumno';
+import Pregunta from './components/preguntaAsistencia';
+import Presenciales from './components/presensiales';
+import Navegacion from './components/navegacion';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="body-panddin">
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Navegacion></Navegacion>
+            <Login></Login>
+          </Route>
+          <Route path='/home'>
+            <Header></Header>
+            <Home></Home>
+          </Route>
+          <Route path='/presenciales'>
+            <Header></Header>
+            <Presenciales></Presenciales>
+          </Route>
+          <Route path='/asistira'>
+              <Navegacion></Navegacion>
+              <AsisAlumno></AsisAlumno>
+          </Route>
+          <Route path='/asistencia/pregunta/:rut'>
+              <Navegacion></Navegacion>
+              <Pregunta></Pregunta>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </Container>
   );
 }
 
