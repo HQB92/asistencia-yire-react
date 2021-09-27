84f8dcfa4809fbb284b27df2f699d5df24e4f8e9
HQB92 <76698088+HQB92@users.noreply.github.com>
2021-09-27 14:30:14 -0300

    Update asis-alumno.js

--- src/components/inicio/asis-alumno.js
+++ src/components/inicio/asis-alumno.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import{Link} from 'react-router-dom'
 import { Form, Container, Row, Col} from 'react-bootstrap';
+import Button from '@restart/ui/esm/Button';
 
 const AsisAlumno= () =>{
     const [rut, rutAlumno]= useState('');
@@ -22,8 +23,8 @@ const AsisAlumno= () =>{
                             <Form.Label>Ingresear Rut Alumno</Form.Label>
                             <Form.Control type="text" id="rut" name="rut" placeholder="11111111-1" onChange={rutChange}/>
                         </Form.Group>
-                        <Link to={`/pregunta/${rut}`} className="button btn btn-primary">Ingresar</Link>
                     </Form>
+                    <Button to={`/pregunta/${rut}`} className="button btn btn-primary">Ingresar</Button>
                 </Col>
                 <Col xs={4} md={2}></Col>
             </Row>
